a23217ea99a3bc73a476b3f93eafc9d4695fda3c
Adombang Munang Mbomndih <dzedock@yahoo.com>
2018-05-10 22:37:55 +0300

    switch to promises

--- app/controllers/appRoutes.js
+++ app/controllers/appRoutes.js
@@ -4,15 +4,17 @@ const express = require('express'),
       config = require(path.normalize(`${__dirname}/../../config/credentials`)),
       { textResponse } = require(path.normalize(`${config.root}/utils/bsUtils`)),
       { renderError } = require(path.normalize(`${config.root}/utils/bsUtils`)),
-      Article = require('../models/sampleModel');
+      article = require('../models/sampleModel');
 
 module.exports = (app) => app.use('/', router);
 
 router.get('/', (req, res, next) => {
-  // const articles = [new Article(), new Article()];
-  res.render('homePage', {
-    title: 'Bomdi Zane',
-    stylesheet: '/css/homePage.min.css',
-    // articles: articles
-  });
+  article().then(results => {
+    res.render('homePage', {
+      title: 'Bomdi Zane - App Routes',
+      stylesheet: '/css/homePage.min.css',
+      articles: results
+    });
+  })
+  .catch(e => console.log(e));
 });
--- app/models/sampleModel.js
+++ app/models/sampleModel.js
@@ -1,8 +1,12 @@
-function Article (opts) {
-    if(!opts) opts = {};
-    this.title = opts.title || 'Bomdi Zane';
-    this.url = opts.url || 'dzedock@gmail.com';
-    this.text = opts.text || 'DON\'T write to my gmail :)';
+function Article (options) {
+    return new Promise((resolve, reject) => {
+        if(!options) options = {};
+        this.title = options.title || 'Bomdi Zane';
+        this.url = options.url || 'dzedock@gmail.com';
+        this.text = options.text || 'DON\'T write to my gmail :)';
+
+        resolve(options);
+    });
 }
 
 module.exports = Article;
